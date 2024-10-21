27cc6f2e7b39ee8650bd815e01393523b38ac27a
PKRankson <siisirankson@gmail.com>
2024-10-21 17:54:44 +0000

    smsapp

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import CloseIcon from '@mui/icons-material/Close';
 import './App.css';
 
 function App() {
-  // State variables for phone number, message, alert, and alert type
+  
   const [phoneNumber, setPhoneNumber] = useState('');
   const [message, setMessage] = useState('');
   const [alertTitle, setAlertTitle] = useState('');
   const [alertMessage, setAlertMessage] = useState('');
   const [alertType, setAlertType] = useState(''); 
   const [open, setOpen] = React.useState(false);
-  const authKey = process.env.REACT_APP_AUTH_KEY; // Add your auth key here
+  const authKey = process.env.REACT_APP_AUTH_KEY; // Auth key
 
   const [deliveryStatus, setDeliveryStatus] = useState('')
 
 
+// Connect to the WebSocket server
   useEffect(() => {
-    // Connect to the WebSocket server (Backend running on localhost:8000)
+    
     const ws = new WebSocket('ws://localhost:8000');
 
-    // Listen for WebSocket messages from the backend
     ws.onmessage = (event) => {
       const status = JSON.parse(event.data);  // Parse the incoming status message
       console.log('Delivery status received from backend:', status);
@@ -39,23 +39,21 @@ function App() {
     };
 
     return () => {
-      ws.close(); // Clean up WebSocket connection when component unmounts
+      ws.close(); 
     };
   }, []);
 
 
   // Handle the form submission
   const handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent the form from refreshing the page
+    event.preventDefault(); 
     
-
-    // Prepare the request body
     const data = {
       "key": authKey,
       "msisdn": phoneNumber,
       "message": message,
       "sender_id": "Test",
-      "callback_url": "https://e5df-154-160-0-196.ngrok-free.app"
+      "callback_url": "https://smsapp-3enu.onrender.com"
     };
 
     try {
@@ -67,29 +65,27 @@ function App() {
         body: JSON.stringify(data),
       });
 
-      // Check if the response is successful
       if (response.ok) {
         const result = await response.json();
         setAlertTitle('Delivered');
-        setAlertMessage(`SMS sent successfully! ${result.job_id} ${result.msisdn} ${result.status} ` ); // Set success message
-        setAlertType('success'); // Set alert type to success
+        setAlertMessage(`SMS sent successfully! ${result.job_id} ${result.msisdn} ${result.status} ` ); 
+        setAlertType('success'); 
         setOpen(true);
-        console.log(result.job_id); // Log the response from the server
+        console.log(result.job_id); 
       } else {
         setAlertTitle('Failed');
         setAlertMessage('Failed to send SMS. Please try again!');
-        setAlertType('error'); // Set alert type to error
+        setAlertType('error'); 
         setOpen(true);
       }
     } catch (error) {
       console.error('Error:', error);
       setAlertTitle('Error');
       setAlertMessage('An error occurred. Please try again.');
-      setAlertType('error'); // Set alert type to error
+      setAlertType('error'); 
       setOpen(true);
     }
 
-    // Clear input fields
     setPhoneNumber('');
     setMessage('');
   };
